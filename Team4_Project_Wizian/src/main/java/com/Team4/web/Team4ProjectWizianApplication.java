f94d62636a400bbc799b5eb66690aa02f16dbe20
nicefriend123 <@gmail.com>
2024-05-07 17:16:55 +0900

    Team4_Proejct

---
+++ Team4_Project_Wizian/src/main/java/com/Team4/web/Team4ProjectWizianApplication.java
@@ -0,0 +1,13 @@
+package com.Team4.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team4ProjectWizianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team4ProjectWizianApplication.class, args);
+	}
+
+}
---
+++ Team4_Project_Wizian/src/test/java/com/Team4/web/Team4ProjectWizianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Team4.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team4ProjectWizianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
