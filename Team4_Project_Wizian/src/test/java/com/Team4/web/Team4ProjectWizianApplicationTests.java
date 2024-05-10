08ac6ac31f235eb510af35e5f29992c7de5d3542
mojitusa <ryuuzakiumi@naver.com>
2024-05-10 11:49:15 +0900

    테스트 지우기

--- Team4_Project_Wizian/src/test/java/com/Team4/web/Team4ProjectWizianApplicationTests.java
+++ Team4_Project_Wizian/src/test/java/com/Team4/web/Team4ProjectWizianApplicationTests.java
@@ -3,7 +3,7 @@ package com.Team4.web;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class Team4ProjectWizianApplicationTests {
 
 	@Test
