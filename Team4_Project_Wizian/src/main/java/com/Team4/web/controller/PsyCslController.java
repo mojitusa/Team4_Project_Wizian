b17ac6245f2dc3dafb1d7953bc0146a6095b1a78
mojitusa <ryuuzakiumi@naver.com>
2024-05-14 21:03:43 +0900

    심리상담 페이지 2-1

--- Team4_Project_Wizian/src/main/java/com/Team4/web/controller/PsyCslController.java
+++ Team4_Project_Wizian/src/main/java/com/Team4/web/controller/PsyCslController.java
@@ -2,17 +2,20 @@ package com.Team4.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.Team4.web.service.PsyCslService;
 
 @Controller
 public class PsyCslController {
 	
 	@Autowired
-	//PsyCslService 
+	PsyCslService psyCslService;
 	
 	@GetMapping("/psycslapply")
-	public String psyCslApply() {
-		
+	public String psyCslApply(Model model) {
+		model.addAttribute("student", psyCslService.getStudentByUserNo("5010000003"));
 		return "content/psycslapply";
 	}
 
