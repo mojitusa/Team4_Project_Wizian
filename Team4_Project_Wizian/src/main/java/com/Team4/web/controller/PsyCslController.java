97f4fab0d5a91c3ab0f989abb34954e273839de8
mojitusa <ryuuzakiumi@naver.com>
2024-05-18 17:48:31 +0900

    심리상담 신청 컨트롤러 연결

--- Team4_Project_Wizian/src/main/java/com/Team4/web/controller/PsyCslController.java
+++ Team4_Project_Wizian/src/main/java/com/Team4/web/controller/PsyCslController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.Team4.web.service.PsyCslService;
 
@@ -18,5 +21,17 @@ public class PsyCslController {
 		model.addAttribute("student", psyCslService.getStudentByUserNo("5010000003"));
 		return "content/psycslapply";
 	}
-
+	
+	@PostMapping("/psycslapplysubmit")
+	public String psyCslApplySubmit(
+			@RequestBody String jsonData,
+			@RequestParam String phoneNumber
+			
+			
+			) {
+		System.out.println(jsonData);
+		System.out.println(phoneNumber);
+		
+	    return "redirect:/psy"; // 리다이렉트할 URL을 반환
+	}
 }
