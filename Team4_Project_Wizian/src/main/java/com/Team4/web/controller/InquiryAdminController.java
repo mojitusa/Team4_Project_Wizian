e784978c2394a115916d71cd07d10aaaaa1da4de
mini970205 <dhekdals@naver.com>
2024-05-20 09:09:15 +0900

    inquiries

--- Team4_Project_Wizian/src/main/java/com/Team4/web/controller/InquiryAdminController.java
+++ Team4_Project_Wizian/src/main/java/com/Team4/web/controller/InquiryAdminController.java
@@ -6,7 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import com.Team4.web.entity.Inquiry;
+import com.Team4.web.entity.InquiryAdmin;
 import com.Team4.web.service.InquiryService;
 
 @Controller
@@ -25,4 +30,13 @@ public class InquiryAdminController {
         model.addAttribute("inquiries", inquiries);
         return "admin/inquiries";
     }
+    
+    @GetMapping("/admin/inquiries/detail/{id}")
+    public String getInquiryDetail(@PathVariable("id") int inquiryId, Model model) {      
+        Inquiry inquiryDetail = inquiryService.getInquiryById(inquiryId);    
+        model.addAttribute("inquiry", inquiryDetail);      
+        return "admin/inquiriesdetail";
+    }
+    
+    
 }
