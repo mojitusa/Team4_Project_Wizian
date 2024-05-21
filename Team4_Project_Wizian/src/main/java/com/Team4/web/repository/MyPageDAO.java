48611728652b6e6b4caac89bc468ee1b67f79303
mojitusa <ryuuzakiumi@naver.com>
2024-05-21 17:03:02 +0900

    포크 오류

--- Team4_Project_Wizian/src/main/java/com/Team4/web/repository/MyPageDAO.java
+++ Team4_Project_Wizian/src/main/java/com/Team4/web/repository/MyPageDAO.java
@@ -27,7 +27,22 @@ public class MyPageDAO {
 	public List<Inquiry> getAllBoard(String userNo) {
 		return sqlSession.selectList("getAllBoard",userNo);
 	}
-	
-	
+
+	public Map<String, Object> getStudInfo(String userNo) {
+		return sqlSession.selectOne("Mymapper.getStudInfo", userNo);
+	}
+
+	public boolean checkPassword(String originalPassword, String userNo) {
+		Map<String, Object> parameters = new HashMap<>();
+	    parameters.put("password", originalPassword);
+	    parameters.put("userNo", userNo);
+		
+		return sqlSession.selectOne("checkPw", parameters);
+	}
+
+	public int updatePw(Map<String, Object> parameters) {
+		
+		return sqlSession.update("updatePw", parameters);
+	}
 	
 }
